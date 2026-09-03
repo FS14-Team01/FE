cc80ef785bb8ee2621b89188518fa25fdf1a2390
yyjinjin <yyjinjin111@naver.com>
2026-09-03 11:32:50 +0900

    feat: React Query 공통 QueryClient 설정 추가

--- src/lib/query-client.js
+++ src/lib/query-client.js
@@ -1,15 +1,21 @@
 import { QueryClient } from "@tanstack/react-query";
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 60 * 1000, // 1분 동안 최신 데이터로 취급
-      gcTime: 300 * 1000,   // 사용하지 않는 캐시도 5분간 보관
+      staleTime: 60 * 1000,
+      gcTime: 300 * 1000,
       refetchOnWindowFocus: false,
 
       retry: (failureCount, error) => {
         return error.status >= 500 && failureCount < 1;
       },
     },
-
-  }
+    
+    mutations: {
+      retry : false
+    },
+  },
 });
+
+export default queryClient;
