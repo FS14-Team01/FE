16ba1126a165e1b666b81cf05e00b464941d2c28
yein0528 <o3o_sumyeong@naver.com>
2026-09-03 18:19:46 +0900

    fix: 푸터 위치 수정 및 공통 레이아웃 a 태그 decoration 삭제

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,19 +1,13 @@
 import './globals.css'
 import Footer from '@/components/common/Footer/Footer'
 
-export const metadata = {
-  title: {
-    default: '최애의 포토',
-    template: '%s | 최애의 포토',
-  },
-  description: '나만의 디지털 포토카드를 만들고 거래하는 플랫폼',
-}
-
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
-      <Footer />
+      <body>
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
