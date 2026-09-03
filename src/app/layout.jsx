7009178e7f585eea4403b8001bc74b1f5fa56de0
yein0528 <o3o_sumyeong@naver.com>
2026-09-03 13:31:46 +0900

    feat: 공통 레이아웃 구현

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,12 +1,16 @@
 import './globals.css';
 
 export const metadata = {
-  title: '최애의 포토',
+  title: {
+    default: '최애의 포토',
+    template: '%s | 최애의 포토',
+  },
+  description: '나만의 디지털 포토카드를 만들고 거래하는 플랫폼',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='ko'>
+    <html lang="ko">
       <body>{children}</body>
     </html>
   );
