93437223fac1a21e585d598b3a15a7ef3cd52afd
yein0528 <o3o_sumyeong@naver.com>
2026-09-04 11:08:09 +0900

    fix: 테스트파일 삭제 및 헤더/푸터 레이아웃 통일

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -1,15 +1,9 @@
 import Header from '@/components/common/Header/Header'
 
-// TODO: 인증 기능 연결 후 실제 사용자 정보로 교체
-const mockUser = {
-  nickname: '유디',
-  points: 1540,
-}
-
 export default function MainLayout({ children }) {
   return (
     <>
-      <Header user={mockUser} />
+      <Header />
       <main>{children}</main>
     </>
   )
--- src/app/photo-cards/create/page.jsx
+++
@@ -1,3 +0,0 @@
-export default function CreatePhotoCardPage() {
-  return null;
-}
