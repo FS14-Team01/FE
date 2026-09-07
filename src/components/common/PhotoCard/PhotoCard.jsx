89681031e5a08ebf1cf1f126ffda06180d5d1e94
yyjinjin <yyjinjin111@naver.com>
2026-09-07 17:42:40 +0900

    feat: PhotoCard 컴포넌트 구현

--- src/components/common/PhotoCard/PhotoCard.jsx
+++ src/components/common/PhotoCard/PhotoCard.jsx
@@ -10,9 +10,15 @@ export default function PhotoCard(
     initialQuantity,
     remainingQuantity,
     status,
-    sellerNickname,
+    showPrice = true
   }
 ) {
+  const gradeClass = {
+    COMMON: styles.gradeCommon,
+    RARE: styles.gradeRare,
+    SUPER_RARE: styles.gradeSuperRare,
+    LEGENDARY: styles.gradeLegendary,
+  };
   return (
     <div className={styles.photoCard}>
       <div className={styles.imgWrap}>
@@ -20,45 +26,53 @@ export default function PhotoCard(
           src={imageUrl}
           alt={name}
         />
-        {/* soldOut일 때 */}
-        {/* <div className={styles.soldOut}>
-          <img src="../assets/ic_soldout.svg" alt="ic_soldout" />
-          <div className={styles.dim}></div>
-        </div> */}
+        {/* soldout일때만 보임 */}
+        {status === "SOLD_OUT" && (
+          <div className={styles.soldOut}>
+            <img src="../assets/ic_soldout.svg" alt="ic_soldout" />
+            <div className={styles.dim}></div>
+          </div>
+        )}
       </div>
       <div className={styles.textWrap}>
         <div className={styles.name}>
-          {name} 이름
+          {name}
         </div>
-        <div className={styles.InfoWrap}>
-          <div className="gradeCategory">
-            <div className={styles.grade}>
-              {grade}등급
+        <div className={styles.infoWrap}>
+          <div className={styles.gradeCategory}>
+            <div className={gradeClass[grade]}>
+              {grade?.replace("_", " ")}
+            </div>
+            <div className={styles.line}>
+
             </div>
             <div className={styles.category}>
-              {category}카테고리
+              {category?.replace("_", " ")}
             </div>
           </div>
-          <div className="ownerName">
+          <div className={styles.ownerName}>
             미쓰손
           </div>
         </div>
         <div className={styles.saleInfo}>
           {/* 마이 갤러리에선 가격 부분 안 보임 */}
-          <div className={styles.price}>
-            <div className={styles.title}>
-              가격
+          {showPrice && (
+            <div className={styles.price}>
+              <div className={styles.title}>
+                가격
+              </div>
+              <div className={styles.data}>
+                {price} P
+              </div>
             </div>
-            <div className={styles.data}>
-              
-            </div>
-          </div>
+          )}
+
           <div className={styles.supply}>
             <div className={styles.title}>
               잔여
             </div>
             <div className={styles.data}>
-              
+              {remainingQuantity}<span> / {initialQuantity}</span>
             </div>
           </div>
         </div>
