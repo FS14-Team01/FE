7da075d3dfcf83cc166ebed461db914066cf0463
이강연 <dbwldhkd01@naver.com>
2026-09-07 18:45:39 +0900

    fix: 드롭다운 '판매 취소' 항목 삭제 (#59)
    * feat: Dropdown 컴포넌트 추가
    
    * feat: Dropdown 컴포넌트 구현
    
    * feat: 공통 dropdown 컴포넌트 구현
    
    * chore: 이전 경로의 Dropdown 파일 삭제
    
    * fix: 태두리 색상 변경
    
    * fix: '판매 취소' 필터 옵션 삭제
    
    ---------
    
    Co-authored-by: yoney0 <yongyong1617@gmail.com>

--- src/components/common/Dropdown/dropdownOptions.js
+++ src/components/common/Dropdown/dropdownOptions.js
@@ -20,7 +20,13 @@ export const CATEGORY_OPTIONS = [
 export const SALE_STATUS_OPTIONS = [
   { value: 'ON_SALE', label: '판매중' },
   { value: 'SOLD_OUT', label: '판매 완료' },
-  { value: 'CANCELLED', label: '판매 취소' },
+];
+
+/** 마켓플레이스 목록 정렬 */
+export const MARKET_SORT_OPTIONS = [
+  { value: 'recent', label: '최신순' },
+  { value: 'priceAsc', label: '낮은 가격순' },
+  { value: 'priceDesc', label: '높은 가격순' },
 ];
 
 /** 마켓플레이스 목록 정렬 */
