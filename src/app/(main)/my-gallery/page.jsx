5cc3266696006586aa3bf0e890f6faefcb44a1ee
yyjinjin <yyjinjin111@naver.com>
2026-09-09 19:01:16 +0900

    feat: 마이갤러리 페이지 UI 구현

---
+++ src/app/(main)/my-gallery/page.jsx
@@ -0,0 +1,12 @@
+import ResponsiveHeader from "@/components/common/ResponsiveHeader/ResponsiveHeader";
+import MyGalleryPage from "@/features/my-gallery/components/MyGalleryPage/MyGalleryPage";
+
+export default function MyGalleryRoute() {
+
+  return (
+    <>
+      <ResponsiveHeader title="마이 갤러리" />
+      <MyGalleryPage/>
+    </>
+  );
+}
