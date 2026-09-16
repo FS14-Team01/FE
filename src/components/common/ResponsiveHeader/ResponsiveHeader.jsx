28732916864a0e7cf7692ea74700c4e68de6c8db
yein0528 <o3o_sumyeong@naver.com>
2026-09-16 09:29:34 +0900

    Revert "fix: 반응형 헤더 인증 정보 연결"
    This reverts commit 9d64991cf32d4d1d225d54374c08eb89ef21d298.

--- src/components/common/ResponsiveHeader/ResponsiveHeader.jsx
+++ src/components/common/ResponsiveHeader/ResponsiveHeader.jsx
@@ -1,28 +1,22 @@
 "use client";
 
-import AuthHeader from "@/components/common/AuthHeader/AuthHeader";
+import Header from "@/components/common/Header/Header";
 import MobilePageHeader from "@/components/common/MobilePageHeader/MobilePageHeader";
 import styles from "./ResponsiveHeader.module.css";
 
 export default function ResponsiveHeader({
   title,
-  points,
-  canUseRandomBox,
-  onRandomBoxClick,
+  user = null,
+  onLogout,
   onNotificationClick,
-  onNotificationClose,
-  notificationPanel,
 }) {
   return (
     <>
       <div className={styles.defaultHeader}>
-        <AuthHeader
-          points={points}
-          canUseRandomBox={canUseRandomBox}
-          onRandomBoxClick={onRandomBoxClick}
+        <Header
+          user={user}
+          onLogout={onLogout}
           onNotificationClick={onNotificationClick}
-          onNotificationClose={onNotificationClose}
-          notificationPanel={notificationPanel}
         />
       </div>
       <div className={styles.mobileHeader}>
