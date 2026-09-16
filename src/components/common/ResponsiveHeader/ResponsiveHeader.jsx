cba1da5ec5f114501c602735c25605b4800fca73
youngsecondbrain-ai <youngsecondbrain-ai   >
2026-09-16 16:23:28 +0900

    refactor: ResponsiveHeader의 불필요한 prop 제거

--- src/components/common/ResponsiveHeader/ResponsiveHeader.jsx
+++ src/components/common/ResponsiveHeader/ResponsiveHeader.jsx
@@ -4,11 +4,11 @@ import AuthHeader from "../AuthHeader/AuthHeader";
 import MobilePageHeader from "@/components/common/MobilePageHeader/MobilePageHeader";
 import styles from "./ResponsiveHeader.module.css";
 
-export default function ResponsiveHeader({ title, onNotificationClick }) {
+export default function ResponsiveHeader({ title }) {
   return (
     <>
       <div className={styles.defaultHeader}>
-        <AuthHeader onNotificationClick={onNotificationClick} />
+        <AuthHeader />
       </div>
       <div className={styles.mobileHeader}>
         <MobilePageHeader title={title} />
