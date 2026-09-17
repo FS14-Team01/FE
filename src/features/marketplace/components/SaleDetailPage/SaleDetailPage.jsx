b861552bc30a3e4fcaa3e6fbd53f0befdd5539d9
이강연 <dbwldhkd01@naver.com>
2026-09-17 16:04:54 +0900

    fix: 마이갤러리 무한스크롤 캐시 무효화 누락 수정 (#151)

--- src/features/marketplace/components/SaleDetailPage/SaleDetailPage.jsx
+++ src/features/marketplace/components/SaleDetailPage/SaleDetailPage.jsx
@@ -150,7 +150,7 @@ export default function SaleDetailPage({ saleId }) {
             });
 
             queryClient.invalidateQueries({
-              queryKey: galleryKeys.lists(),
+              queryKey: galleryKeys.all,
             });
 
             queryClient.invalidateQueries({
