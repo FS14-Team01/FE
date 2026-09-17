044883bcf0994d72cb35c234a9bc4fa73ee68c18
윤여진 <yyjinjin111@gmail.com>
2026-09-17 16:33:39 +0900

    fix: 마이갤러리 리뷰 및 생성 접근 제어 반영

--- src/app/(protected)/my-gallery/page.jsx
+++ src/app/(protected)/my-gallery/page.jsx
@@ -1,5 +1,11 @@
+import ResponsiveHeader from "@/components/common/ResponsiveHeader/ResponsiveHeader";
 import MyGalleryPage from "@/features/my-gallery/components/MyGalleryPage/MyGalleryPage";
 
 export default function MyGalleryRoute() {
-  return <MyGalleryPage />;
+  return (
+    <>
+      <ResponsiveHeader title="마이 갤러리" />
+      <MyGalleryPage />
+    </>
+  );
 }
